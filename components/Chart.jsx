20b8d74c8aab10677f37cfc1963a314772e65b9e
Shashwat <shashwat.rai.mec20@itbhu.ac.in>
2024-03-08 13:03:05 +0530

    Updated the chart component

--- components/Chart.jsx
+++ components/Chart.jsx
@@ -6,30 +6,30 @@ function Chart() {
   const container = useRef();
 
   useEffect(() => {
-    return () => {
-      const script = document.createElement("script");
-      script.src =
-        "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
-      script.type = "text/javascript";
-      script.async = true;
-      script.innerHTML = `
-        {
-          "width": "100%",
-          "height": "100%",
-          "symbol": "BITSTAMP:BTCUSD",
-          "interval": "D",
-          "timezone": "Etc/UTC",
-          "theme": "light",
-          "style": "3",
-          "locale": "en",
-          "enable_publishing": false,
-          "allow_symbol_change": true,
-          "calendar": false,
-          "hide_volume": true,
-          "support_host": "https://www.tradingview.com"
-        }`;
-      container.current.appendChild(script);
-    };
+    const script = document.createElement("script");
+    script.src =
+      "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
+    script.type = "text/javascript";
+    script.async = true;
+    script.innerHTML = `
+      {
+        "width": "100%",
+        "height": "100%",
+        "symbol": "BITSTAMP:BTCUSD",
+        "interval": "D",
+        "timezone": "Etc/UTC",
+        "theme": "light",
+        "style": "3",
+        "locale": "en",
+        "enable_publishing": false,
+        "allow_symbol_change": true,
+        "calendar": false,
+        "hide_volume": true,
+        "support_host": "https://www.tradingview.com"
+      }`;
+    container.current.appendChild(script);
+    // return () => {
+    // };
   }, []);
 
   return (
